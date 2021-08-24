00992d36575c4bb89123df6b20229b7866b521f7
nikoagge <nikoagge@users.noreply.github.com>
2021-08-24 10:17:30 +0300

    Initial Commit

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Nikos Aggelidis on 24/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
